17349e2d819ad37c8d5e7c99ba3c4e8cdfab7b5f
Pratik <j.pratik025@gmail.com>
2026-02-25 21:17:34 +0530

    feat: deployed backend to railway with fronent connection

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios'
 import type { ApiResponse } from '../types'
 
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
-
+console.log(import.meta.env)
+console.log(import.meta.env.VITE_API_URL)
 // Fetch portfolio data from the API
 export async function fetchPortfolio(): Promise<ApiResponse> {
     const response = await axios.get<ApiResponse>(`${BASE_URL}/api/stocks`)
